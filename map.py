c38f2761d924653239a80e6fd2601e88c47eb3f2
Dumebi Osadebe <winifred2412@gmail.com>
2022-07-05 13:41:18 -0400

    function to generate colour for the volcanoe locator based on the status of the volcanoe

--- map.py
+++ map.py
@@ -10,9 +10,38 @@ fg = folium.FeatureGroup(name = "My Map")
 lat = list(vol["LAT"])
 lon = list(vol["LON"])
 name = list(vol["NAME"])
+stat = list(vol["STATUS"])
 
-for i, j, k in zip(lat, lon, name):
-    fg.add_child(folium.Marker(location=[i, j], popup = k, icon=folium.Icon(color = "darkred")))
+statuses = pd.unique(stat) #unique statuses fount in the "STATUS" column of the valcanoe dataframe
+
+#generates colour for the volcanoe marker based on the stsus provided in the volcanoe.txt
+def color_gen(status):
+    if status == "Historical":
+        return "purple"
+    elif status == "Tephrochronology":
+        return "orange"
+    elif status == "Dendrochronology":
+        return "darkred"
+    elif status == "Radiocarbon":
+        return "pink"
+    elif status == "Varve Count":
+        return "gray"
+    elif status == "Holocene?":
+        return "lightgreen"
+    elif status == "Holocene":
+        return "green"
+    elif status == "Pleistocene-Fumarolic":
+        return "darkblue"
+    elif status == "Anthropology":
+        return "cadetblue"
+    else:
+        return "white"
+
+#pop-up uses name of volcanoe instead of name of elev in the original course
+for i, j, n, st in zip(lat, lon, name, stat):
+    color = color_gen(st)
+    fg.add_child(folium.Marker(location=[i, j], popup = n, icon=folium.Icon(color = color)))
+
     
     
 map.add_child(fg)
